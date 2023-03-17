c689a88eb7706f4ae5cd52f919025f680ecc9df7
ozelbaz <oz.elbaz@gmail.com>
2023-03-17 17:38:53 +0200

    Created the first two classes (Robot and Env)
    Opened a dev branch for better tracking of changes

---
+++ main.py
@@ -0,0 +1,36 @@
+# DWA navigation Simulation
+
+# Import necessary libraries and modules
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+# Define classes and functions for the simulation
+
+class Robot:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vx = 0
+        self.vy = 0
+        self.ax = 0
+        self.ay = 0
+
+    def update(self, dt): # Update the robot position and velocities based on the acceleration and time interval
+        # Velocity update
+        self.vx = self.vx + self.ax * dt
+        self.vy = self.vy + self.ay * dt
+        # Position update
+        self.x = self.x + self.vx * dt
+        self.y = self.y + self.vy * dt
+
+class Enviourment:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.obstacles = []
+
+    def add_obstacle(self, obstacle):
+        # Add an obstacle to the environment
+        self.obstacles.append(obstacle) # obstacle might need to be a class of itself?
