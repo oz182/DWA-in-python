1fb1e98ec565cfaf852d3131019db027bd007bde
ozelbaz <oz.elbaz@gmail.com>
2023-03-20 01:12:14 +0200

    Dynamic window function.

--- main.py
+++ main.py
@@ -5,11 +5,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-
-
+import math
 
 
 # Define classes and functions for the simulation
+
+dt = 0.1 # need to think about the right place for this value. and to make it a constant
 
 class Robot:
 
@@ -27,7 +28,12 @@ class Robot:
         self.ay = 0
         self.W = 0
 
-    def update(self, dt): # Update the robot position and velocities based on the acceleration and time interval
+        self.Vmax = 2
+        self.Wmax = 1
+        self.acc_max = 1
+        self.omegaMax = 1
+
+    def update(self, dt):  # Update the robot position and velocities based on the acceleration and time interval
         # Velocity update
         self.vx = self.vx + self.ax * dt
         self.vy = self.vy + self.ay * dt
@@ -38,7 +44,6 @@ class Robot:
         self.theta = self.theta + self.W * dt
 
 
-
 class Env:
     def __init__(self, width, height):
         self.width = width
@@ -47,18 +52,7 @@ class Env:
 
     def add_obstacle(self, obstacle):
         # Add an obstacle to the environment
-        self.obstacles.append(obstacle) # obstacle might need to be a class of itself? What is an obstacle?
-
-
-
-class DWA_Parameters:
-    def __init__(self, Sigma, Heading, Obs_avoidance, Speed):
-        self.Sigma = Sigma
-        self.Heading = Heading
-        self.Obs_avoidancec = Obs_avoidance
-        self.Speed = Speed
-
-
+        self.obstacles.append(obstacle)  # obstacle might need to be a class of itself? What is an obstacle?
 
 
 def animate(robot, env):
@@ -76,23 +70,33 @@ def animate(robot, env):
         robot.update(dt=0.1)
         ax.clear()
         for obstacle in env.obstacles:
-            #ax.add_patch(obstacle.get_patch())  # Not clear what is get_patch??
+            # ax.add_patch(obstacle.get_patch())  # Not clear what is get_patch??
             pass
-        #ax.add_patch(robot.get_patch())
+        # ax.add_patch(robot.get_patch())
         plt.draw()
         plt.pause(0.001)
 
 
+# Define the weights od the DWA algorithm
 
-def dwa_algo(robot, env):
+HEADING = 1
+SPEED = 1
+AVOIDANCE = 1
+SIGMA = 1
 
-    # Starting with the construction of the dynamic window
+def dynammic_window(robot, dt):
 
-
+    Streight_Vel = np.arange(start=-robot.Vmax,stop=robot.Vmax,step=robot.acc_max*dt)
 
 
     pass
 
+
+def dwa_planner(robot, env):
+    # Starting with the construction of the dynamic window
+
+
+    pass
 
 
 # Main code for the simulation
@@ -100,15 +104,19 @@ def dwa_algo(robot, env):
 def main():
     # Create a new environment and add obstacles to it
 
-    envFrame = Env(10,10)
-    #env.add_obstacle(RectangleObstacle(x))
+    envFrame = Env(10, 10)
+    # env.add_obstacle(RectangleObstacle(x))
 
-    robotProt = Robot(0.5,0.5,45,1,1)
+    robot_proto = Robot(0.5, 0.5, 45, 1, 1)
 
-    animate(robotProt,envFrame)
+    #animate(robot_proto, envFrame)
 
     print("Hello world!")
+
+    print(np.arange(start=1,stop=10,step=0.5))
+
     pass
+
 
 if __name__ == "__main__":
     main()
